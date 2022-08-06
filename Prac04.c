f261d259118b05ba00a596000baa54290163a48f
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-06 20:55:24 +0300

    Update Prac04.c

--- Prac04.c
+++ Prac04.c
@@ -1,3 +1,11 @@
+/*
+--------------
+Name: Task01.c
+--------------
+Description: Write a program, that formats a string with string
+variable and 2 float variables, then prints them into the console.
+--------------
+*/
 #include <stdio.h>
 #define BOOK "Kobzar"
 int main(){
@@ -5,4 +13,5 @@ int main(){
 	float percent = 80.0;
 	printf("This copy of %s sells for %.2f\n", BOOK, cost);
 	printf("That is %2.f %% of list",percent);
+	return 0;
 }
