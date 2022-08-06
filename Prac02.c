a862a2de5d7d4e7998c739f85a38ecb86717331e
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-06 21:36:40 +0300

    Update Prac02.c

--- Prac02.c
+++ Prac02.c
@@ -1,7 +1,15 @@
+/*
+--------------
+Name: Task02.c
+--------------
+Description: Write a program, that uses 'for' loop
+and prints out the result.
+--------------
+*/
 #include <stdio.h>
 int main(void){
 	int value;
-	for(value = 36; value>0; value /=2){
+	for(value = 36; value > 0; value /= 2){
 		printf("%3d",value);
 	}
 	return 0;
