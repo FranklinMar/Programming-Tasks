1103dbaa460c515afe0dd97c6f3f3d752f616caf
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-06 21:34:48 +0300

    Update and rename Prac01.c to Task01.c

---
+++ Task01.c
@@ -0,0 +1,23 @@
+/*
+--------------
+Name: Task01.c
+--------------
+Description: Write a program, that demonstrates
+usage of different operators with assigning.
+--------------
+*/
+#include <stdio.h>
+int main(void){
+	int t = 2;
+	t += 5;
+	printf("The value of t:%d\n", t);
+	t *= 10;
+	printf("The value of t:%d\n", t);
+	t -= 6;
+	printf("The value of t:%d\n", t);
+	t /= 8;
+	printf("The value of t:%d\n", t);
+	t %= 3;
+	printf("The value of t:%d\n", t);
+	return 0;
+}
