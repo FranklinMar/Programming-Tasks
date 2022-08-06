9cf808241608dbe7bc308f8e09e80cc85803315c
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 00:39:22 +0300

    Update P2.c

--- P2.c
+++ P2.c
@@ -1,8 +1,16 @@
+/*
+--------------
+Name: Task04A.c
+--------------
+Description: Write a program, that prints
+string by every character with a loop.
+--------------
+*/
 #include <stdio.h>
 
 int main(void){
 	char a[27] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	for(int i = 0;i < sizeof(a);i++){
+	for (int i = 0; i < sizeof(a); i++) {
 		putchar(a[i]);
 	}
 	return 0;
