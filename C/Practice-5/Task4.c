89609523377ceb7913fde018da0096ec44eb0120
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 00:52:13 +0300

    Update and rename P4.c to C/Practice-5/Task4.c

---
+++ C/Practice-5/Task4.c
@@ -0,0 +1,24 @@
+/*
+--------------
+Name: Task03.c
+--------------
+Description: Write a program, that reads 
+a character from user, and checks if it is space, alphabetic, 
+or non-alphabet character, then prints to the console.
+--------------
+*/
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+	printf("Enter symbol:\n");
+	char a = getchar();
+	if (isalpha(a)) {
+		printf("%c is an alphabet",a);
+	} else if(isspace(a)) {
+		printf("SPACE was entered");
+	} else {
+		printf("%c is not an alphabet",a);
+	}
+	return 0;
+}
