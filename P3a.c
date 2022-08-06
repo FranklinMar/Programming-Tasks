2f3852c38bb91daa1b6d0602e39204fc80c2ff13
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 00:48:59 +0300

    Update P3a.c

--- P3a.c
+++ P3a.c
@@ -1,25 +1,33 @@
+/*
+--------------
+Name: Task03A.c
+--------------
+Description: Write a program, that reads a 
+string from user, then converts every character
+of it to upper (if possible) and prints it on console.
+--------------
+*/
 #include <stdio.h>
 #include <ctype.h>
 
 int main(void){
-	int k = 'a' - 'A';//'a' - 'A' = 32 По таблиці ASCII
+	int k = 'a' - 'A';// = 32 According to ASCII coding
 	int S = 28;
 	int i = 0;
 	char a[S];
 	printf("Enter symbols:\n");
-	// printf("%d %d %d %d\n",'A','Z','a','z');
-	while((a[i] = getchar()) != '\n' && i < sizeof(a)){
+	while( (a[i] = getchar()) != '\n' && i < sizeof(a) ){
 		i++;
 	}
 	printf("Result:\n");
-	for(int j=0;j < i;j++){
-		if(islower(a[j])){
-			putchar( a[j] );
-		}else if(a[j] >='A' && a[j] <= 'Z'){// if(isupper(a[j])
-			a[j] = (int)a[j] + k;
-			putchar((char)a[j]);
-		}else{
-			putchar(a[j]);	
+	for(int j = 0; j < i; j++){
+		if (islower(a[j])) {
+			putchar(a[j]);
+		} else if (a[j] >= 'A' && a[j] <= 'Z'){// isupper(a[j])
+			a[j] = (int) a[j] + k;
+			putchar((char) a[j]);
+		} else {
+			putchar(a[j]);
 		}
 	}
 	return 0;
