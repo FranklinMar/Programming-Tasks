ba94e73de627481ea6113b96c4ce710736846cff
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-06 20:42:20 +0300

    Update Task01.c

--- C/Practice-1/Task01.c
+++ C/Practice-1/Task01.c
@@ -1,9 +1,17 @@
+/*
+--------------
+Name: Task01.c
+--------------
+Description: Write a program, that asks user for temperature in Celsius, then 
+converts it into Fahrenheit scale, and then prints it out into the console.
+--------------
+*/
 #include <stdio.h>
 int main(void) {
 	float cel, far;
 	printf("Enter temperature in C :");
 	scanf("%f", &cel);
-	far = cel * 9 / 5 + 32; // Формула для переведення температури в Фаренгейти
+	far = cel * 9 / 5 + 32; // Formula of converting Celsius temperature into Fahrenheit
 	printf("\n The same temperature in Fahrenheit is: %.2f \n",far);
 	return 0;
 }
