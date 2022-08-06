aed2475aad3f6afd9ba77bdb03ce28fb50cd21b6
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 01:03:32 +0300

    Update and rename P1.c to C/Practice-6/Task01.c

--- C/Practice-6/Task01.c
+++ C/Practice-6/Task01.c
@@ -1,15 +1,31 @@
+/*
+--------------
+Name: Task03.c
+--------------
+Description: Write a program, that will rotate square 3x3 
+to different directions. The direction of rotation must be 
+determined by user, so you should foresee and implement 
+such a possibility.
+--------------
+*/
 #include <stdio.h>
 #include <string.h>
 #define H 3
+
+// Rotate the square to the left
 void rotl(int a[H][H]);
+// Print the square to the console
 void print(int a[H][H]);
+// Flip square vertically
 void fliph(int a[H][H]);
+// Flip square horizontally
 void flipv(int a[H][H]);
-int main(void){
+
+int main(void) {
 	char e;
-	int cube[H][H] = {{1,2,3},{4,5,6},{7,8,9}};
-	for(int i = 0;i<H;i++){
-		for(int j = 0; j < H;j++){
+	int cube[H][H] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < H; j++) {
 			printf(" %d ",cube[i][j]);
 		}
 		printf("\n");
@@ -22,8 +38,8 @@ int main(void){
 	printf("D. Flip vertical\n");
 	printf("E. Flip horizontal\n");
 	printf("Q. Quit\n");
-	while((e = getchar()) != 'Q'){
-	switch(e){
+	while ((e = getchar()) != 'Q') {
+	switch (e) {
 		case 'A':
 			rotl(cube);
 			print(cube);
@@ -47,13 +63,14 @@ int main(void){
 			fliph(cube);
 			print(cube);
 			break;
-	}}
+	}
+	}
 	return 0;
 }
 
 void print(int cube[H][H]){
-	for(int i = 0;i<H;i++){
-		for(int j = 0; j < H;j++){
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < H; j++) {
 			printf(" %d ",cube[i][j]);
 		}
 		printf("\n");
@@ -63,48 +80,48 @@ void rotl(int cube[H][H]){
 	int a[H][H];
 	int e = 0;
 	int f = 0;
-	for(int i = 2; i >= 0;i--){
-		f=0;
-		for(int j = 0; j < H;j++){
+	for (int i = 2; i >= 0; i--) {
+		f = 0;
+		for (int j = 0; j < H; j++) {
 			a[e][f] = cube[j][i];
 			f++;
 		}
 		e++;
 	}
-	for(int i = 0;i<H;i++){
-		for(int j = 0; j < H;j++){
-			cube[i][j]=a[i][j];
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < H; j++) {
+			cube[i][j] = a[i][j];
 		}
 	}
 }
 void fliph(int cube[H][H]){
 	int a[H][H];
 	int f = 0;
-	for(int i = 0; i < H ;i++){
-		f=0;
-		for(int j = 2; j >=0 ;j--){
+	for (int i = 0; i < H; i++) {
+		f = 0;
+		for (int j = 2; j >= 0; j--) {
 			a[i][f] = cube[i][j];
 			f++;
 		}
 	}
-	for(int i = 0;i<H;i++){
-		for(int j = 0; j < H;j++){
-			cube[i][j]=a[i][j];
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < H; j++) {
+			cube[i][j] = a[i][j];
 		}
 	}
 }
 void flipv(int cube[H][H]){
 	int a[H][H];
 	int f = 0;
-	for(int i = 2; i >= 0 ;i--){
-		for(int j = 0; j < H ;j++){
+	for (int i = 2; i >= 0; i--) {
+		for (int j = 0; j < H; j++) {
 			a[f][j] = cube[i][j];
 		}
 		f++;
 	}
-	for(int i = 0;i<H;i++){
-		for(int j = 0; j < H;j++){
-			cube[i][j]=a[i][j];
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < H; j++) {
+			cube[i][j] = a[i][j];
 		}
 	}
 }
