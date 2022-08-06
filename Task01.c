d44da10e82950a326e0a9aa326e05812d1d72a4b
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 00:10:11 +0300

    Update and rename P1.c to Task01.c

--- Task01.c
+++ Task01.c
@@ -1,3 +1,12 @@
+/*
+--------------
+Name: Task01.c
+--------------
+Description: Write a program, that prints a mark ETCS
+information by grade using "switch" operator with "case"s by
+using a table with grades.
+--------------
+*/
 #include <stdio.h>
 #include <stdlib.h>
 int main(void){
@@ -6,39 +15,39 @@ int main(void){
 	char k = getchar();
 	switch(l){
 		case 'A':
-			printf("%c - Відмінно\n",l);
-			printf("Бали за 100-бальною шкалою  Оцінка за шкалою ЄКТС  Оцінка за національною шкалою\n ");
-			printf("       95 … 100                       A                        Відмінно\n ");
+			printf("%c - Відмінно\n",l);// Perfect
+			printf("Бали за 100-бальною шкалою  Оцінка за шкалою ЄКТС  Оцінка за національною шкалою\n ");// Points up to 100 / Grade / Grade by-
+			printf("       95 … 100                       A                        Відмінно\n ");// -National Scale
 			break;
 		case 'B':
-			printf("%c - Добре\n",l);
+			printf("%c - Добре\n",l);// Good
 			printf("Бали за 100-бальною шкалою  Оцінка за шкалою ЄКТС  Оцінка за національною шкалою\n ");
 			printf("       85 … 94                        B                         Добре\n ");
 			break;
 		case 'C':
-			printf("%c - Добре\n",l);
+			printf("%c - Добре\n",l);// Good
 			printf("Бали за 100-бальною шкалою  Оцінка за шкалою ЄКТС  Оцінка за національною шкалою\n ");
 			printf("       75 … 84                        C                         Добре\n ");
 			break;
 		case 'D':
-			printf("%c - Задовільно\n",l);
+			printf("%c - Задовільно\n",l);// Quite Enough
 			printf("Бали за 100-бальною шкалою  Оцінка за шкалою ЄКТС  Оцінка за національною шкалою\n ");
 			printf("       65 … 74                        D                       Задовільно\n ");
 			break;
 		case 'E':
-			printf("%c - Задовільно\n",l);
+			printf("%c - Задовільно\n",l);// Quite Enough
 			printf("Бали за 100-бальною шкалою  Оцінка за шкалою ЄКТС  Оцінка за національною шкалою\n ");
 			printf("       60 … 64                        E                       Задовільно\n ");
 			break;
 		case 'F':
-			if(k =='x'){
-			printf("%c%c - Незадовільно\n",l,k);
-			printf("Бали за 100-бальною шкалою  Оцінка за шкалою ЄКТС  Оцінка за національною шкалою\n ");
-			printf("       менше 60                       Fx                     Незадовільно\n ");
-			}else{
-			printf("%c - Не допущено\n",l);
-			printf("Бали за 100-бальною шкалою          Оцінка за шкалою ЄКТС          Оцінка за національною шкалою\n ");
-			printf("Меньше 60,не виконання умов допуску         F                   Не допущено до семестрового контролю\n");
+			if(k =='x'){// Not Enough
+				printf("%c%c - Незадовільно\n",l,k);
+				printf("Бали за 100-бальною шкалою  Оцінка за шкалою ЄКТС  Оцінка за національною шкалою\n ");
+				printf("       менше 60                       Fx                     Незадовільно\n ");
+			} else {// Not cleared
+				printf("%c - Не допущено\n",l);
+				printf("Бали за 100-бальною шкалою          Оцінка за шкалою ЄКТС          Оцінка за національною шкалою\n ");
+				printf("Меньше 60,не виконання умов допуску         F                   Не допущено до семестрового контролю\n");
 			}
 			break;
 		default:
