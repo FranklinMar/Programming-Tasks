d3f6e229cb1aa86a76ccc6817f9856ffb1622e64
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 01:55:23 +0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,33 +1,46 @@
+/*
+--------------
+Name: Task01.c
+--------------
+Description: Write a program, that will encrypt a string,
+which is read from the user input, by Ceasar's cypher and
+print it out on the console.
+*/
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #define S 18
 #define a 30
+
+// Encrypt the string by Ceasar's cypher
 char cypher(char w[S]);
+
 int main(void){
 	char word[S];
 	int I = 1;
 	printf("Please enter the word to encrypt it:\n");
-	scanf("%s",word);
-	for(int i = 0;i < strlen(word);i++){
-		if(!islower(word[i]) && !isupper(word[i])){
+	scanf("%s", word);
+	for (int i = 0; i < strlen(word); i++) {
+		// If not an alphabet character
+		if (!islower(word[i]) && !isupper(word[i])) {
 			I = 0;
 		}
 	}
-	if(I){
+	if (I) {
 		cypher(word);
 		printf("Encrypted word:%s",word);
-	}else{
+	} else {
 		printf("Sorry,but you have entered wrong type of symbols.\n Please use English Alphabet letters");
 	}
 	return 0;
 }
+
 char cypher(char w[S]){
-	for(int i = 0;i < strlen(w); i++){
-		if(w[i] >= 'x'||(w[i] >= 'X' && w[i] <= 'Z')){
-			w[i]-= 23;
-		}else{
-		w[i] += 3;
+	for (int i = 0; i < strlen(w); i++) {
+		if (w[i] >= 'x' || (w[i] >= 'X' && w[i] <= 'Z')) {
+			w[i] -= 23;// ('z' - 'a') - key = 26 - Key
+		} else {
+			w[i] += 3;// Key
 		}
 	}
 	return w;
