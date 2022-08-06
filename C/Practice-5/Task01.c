d784e0e8dc65dfe7164832dece0698bb0a5a603e
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 00:34:25 +0300

    Update and rename P1.c to C/Practice-5/Task01.c

---
+++ C/Practice-5/Task01.c
@@ -0,0 +1,21 @@
+/*
+--------------
+Name: Task04A.c
+--------------
+Description: Write a program, that reads a string 
+from a user and then prints it by every character.
+--------------
+*/
+#include <stdio.h>
+#define S 16
+int main(void){
+	char w[S];
+	printf("Please enter any few symbols:");
+	gets(w);
+	printf("The symbols are:");
+	for(int i = 0; i < sizeof(w); i++){
+		printf(" ");
+		putchar(w[i]);
+	}
+	return 0;
+}
