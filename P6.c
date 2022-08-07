9c616bfed5942a409886edc713a27d9ca435f437
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 19:05:26 +0300

    Update P6.c

--- P6.c
+++ P6.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 
-int main(void){
+int main(void) {
 	int n;
 	int l = 1;
 	printf("Enter number of elements of array:\n");
-	while(l){
+	while (l) {
 		printf("N:");
-		scanf("%d",&n);
-		if(n<=0){
+		scanf("%d", &n);
+		if(n <= 0) {
 			printf("Number of elements of array can't be negative/0\n");
 			getchar();
-		}else{
-			l=0;
+		} else {
+			l = 0;
 		}
 	}
 	int arr1[n];
 	int arr2[n];
 	int arr3[n];
-	printf("Enter %d elements of array #1:",n);
-	for(int i=0;i<n;i++){
-		scanf("%d",&arr1[i]);
+	printf("Enter %d elements of array #1:", n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr1[i]);
 	}
 	getchar();
-	printf("Enter %d elements of array #2:",n);
-	for(int i=0;i<n;i++){
-		scanf("%d",&arr2[i]);
+	printf("Enter %d elements of array #2:", n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr2[i]);
 	}
 	printf("Array#1:");
-	for(int i=0;i<n;i++){
-			printf("%d ",*(arr1+i));
+	for (int i = 0; i < n; i++) {
+			printf("%d ", *(arr1 + i));
 	}
 	printf("\nArray#2:");
-	for(int i=0;i<n;i++){
+	for (int i = 0; i < n; i++) {
 			printf("%d ",*(arr2+i));
 	}
-	for(int i=0;i<n;i++){
-			*(arr3+i)=*(arr1+i)+*(arr2+i);
+	for (int i = 0; i < n; i++) {
+			*(arr3 + i) = *(arr1 + i) + *(arr2 + i);
 	}
 	printf("\nArray#3:");
-	for(int i=0;i<n;i++){
-			printf("%d ",*(arr3+i));
+	for (int i = 0; i < n; i++) {
+			printf("%d ", *(arr3+i));
 	}
 	return 0;
 }
