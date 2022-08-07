a74dbb71ff048103c746a5ba89b3860bdc06dc4d
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 16:58:57 +0300

    Update and rename P2.c to C/Practice-9/Task02.c

---
+++ C/Practice-9/Task02.c
@@ -0,0 +1,36 @@
+/*
+--------------
+Name: Task02.c
+--------------
+Description: Write a program, that complete
+next tasks with pointers:
+- Get a sentence from user input with letters and symbols;
+- Delete symbols from a string, leaving only spaces 
+and alphabetic characters.
+--------------
+*/
+#include <stdio.h>
+
+int main (void) {
+	char string[30];
+	char *ptr = &string[0];
+	int i = 0;
+	printf("Enter the sentence of 30 symbols and letters:");
+	while ( (*(ptr + i) = getchar()) != '\n' && i < 30) {
+		i++;
+	}
+	for(i = 0; *(ptr + i) != '\0'; ++i){
+		while(!((*(ptr + i) == '\0') || (*(ptr + i) == ' ') || 
+			((*(ptr + i) >= 'a') && (*(ptr + i) <= 'z')) || 
+			((*(ptr + i) >= 'A') && (*(ptr + i) <= 'Z')))) {
+			int j;
+			for(j = i; *(ptr + j) != '\0'; ++j){
+				*(ptr + j) = *(ptr + j + 1);
+			}
+			*(ptr + j)='\0';
+		}
+	}
+	printf("\nSentence:");
+	printf("%s",string);
+	return 0;
+}
