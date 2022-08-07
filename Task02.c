7c2b638689f99cad0dbbd059aaf31df808113f3c
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 18:26:00 +0300

    Update and rename P2.c to Task02.c

--- Task02.c
+++ Task02.c
@@ -1,18 +1,18 @@
+/*
+--------------
+Name: Task02.c
+--------------
+Description: Print out address of 
+array element in a different ways.
+--------------
+*/
 #include <stdio.h>
 
 int main(void){
 	int data [30][100];
 	int *adress = &data[0][0];
-/*	int *add1 = &data[22][56];
-	int *add2 = &data[22][0];
-	int *add3 = &data[0][0];
-	*adress = data;
-	*add1 = &data[22][56];
-	*add2 = &data[22][0];
-	*add3 = &data[0][0];*/
 	printf("data[22][56]:address %p\n",(*(data+22)+56));
 	printf("data[22][56]:address %p\n",&data[22][56]);
-	//printf("data[22][0]:address %p\n",(adress+22)/*(adress+2200)*/);
 	printf("data[22][0]:address %p\n",(data+22));
 	printf("data[22][0]:address %p\n",&data[22][0]);
 	printf("data[0][0]:address %p\n",adress);
