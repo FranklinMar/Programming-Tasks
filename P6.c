95c2a8f3f245ab46e65a2cf6075905bf9bcec560
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 19:14:31 +0300

    Update P6.c

--- P6.c
+++ P6.c
@@ -32,7 +32,7 @@ int main(void) {
 	}
 	printf("\nArray#2:");
 	for (int i = 0; i < n; i++) {
-			printf("%d ",*(arr2+i));
+			printf("%d ", *(arr2 + i));
 	}
 	for (int i = 0; i < n; i++) {
 			*(arr3 + i) = *(arr1 + i) + *(arr2 + i);
