d34ab284b57546a3ad7e881be49d498d5536b5dd
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 16:42:15 +0300

    Update and rename P3.c to Task03.c

---
+++ Task03.c
@@ -0,0 +1,24 @@
+/*
+--------------
+Name: Task03.c
+--------------
+Description: Write a program, that will 
+find the average of elements of given 
+array by using pointers and address.
+--------------
+*/
+#include <stdio.h>
+#define S 20
+int main(){
+	double a[S] = {0.228952, 0.568418, 0.820277, 0.117099, 0.755212,
+			0.509299, 0.572073, 0.224526, 0.852861, 0.0612133,
+			0.175636, 0.568243, 0.0100543, 0.702012, 0.0345108,
+			0.146549, 0.189951, 0.144139, 0.261263, 0.474034};
+	double average;
+	for(int i = 0; i < S; i++)
+		average += *(a + i);
+	printf("Sum:%lf\n", average);
+	average = average / S;
+	printf("Average number:%lf", average);
+	return 0;
+}
