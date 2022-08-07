77c407e8da477b37d296e62612f8bb8e0c2508fa
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 16:21:16 +0300

    Update and rename P6.c to Task06.c

---
+++ Task06.c
@@ -0,0 +1,30 @@
+/*
+--------------
+Name: Task06.c
+--------------
+Description: Write a program, that will encrypt
+a word or message without spaces with .
+--------------
+*/
+#include <stdio.h>
+
+int size(char s[50]) {
+	for (int i = 0; i < 50; i++)
+	if (s[i] > 122 || s[i] < 97)
+		return i;
+	return 49;
+}
+
+int main() {
+	char s[50];
+	int key;
+	printf("Insert a word or text without spaces\n");
+	scanf("%s", s);
+	printf("Insert a diameter\n");
+	scanf("%d", &key);
+	for (int y = size(s) / key - 1; y >= 0; y--)
+		for (int x = 0; x <= key; x++)
+			if(x * (size(s) / key) + y < size(s))
+				printf("%c", s[x * (size(s) / key) + y]);
+	return 0;
+}
