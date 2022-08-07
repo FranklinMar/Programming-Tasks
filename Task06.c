bcbc7f05c63b580abf884573f8dd2c8ecfc16b61
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 16:23:23 +0300

    Update Task06.c

--- Task06.c
+++ Task06.c
@@ -3,7 +3,8 @@
 Name: Task06.c
 --------------
 Description: Write a program, that will encrypt
-a word or message without spaces with .
+a word or message without spaces with key to 
+be determined by user with Scytale cipher.
 --------------
 */
 #include <stdio.h>
