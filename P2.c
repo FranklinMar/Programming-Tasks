3796498c712b649a33e0bbc3857a205dca6def00
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 16:35:04 +0300

    Update P2.c

--- P2.c
+++ P2.c
@@ -1,33 +1,44 @@
+/*
+--------------
+Name: Task02.c
+--------------
+Description: Write a program, that will 
+swap the values of variables in ascending
+and descending order by using pointers and address.
+--------------
+*/
 #include <stdio.h>
+
 void increase(double *a,double *b);
 void decrease(double *a,double *b);
+
 int main(void){
-	// 1 Частина
-	double a,b;
+	// Part 1
+	double a, b;
 	printf("Please enter 2 numbers to swap them in order of increasing:");
-	scanf("%lf %lf",&a,&b);
-	increase(&a,&b);
-	printf("Order of increasing:\n1:%lf\n2:%lf\n",a,b);
-	// 2 Частина
+	scanf("%lf %lf", &a, &b);
+	increase(&a, &b);
+	printf("Order of increasing:\n1:%lf\n2:%lf\n", a, b);
+	// Part 2
 	printf("Please enter 2 numbers to swap them in order of decreasing:");
-	scanf("%lf %lf",&a,&b);
-	decrease(&a,&b);
-	printf("Order of decreasing:\n1:%lf\n2:%lf\n",a,b);
+	scanf("%lf %lf", &a, &b);
+	decrease(&a, &b);
+	printf("Order of decreasing:\n1:%lf\n2:%lf\n", a, b);
 	return 0;
 }
-void increase(double *a,double *b){
+void increase(double *a, double *b){
 	double temp;
-	if(*a>*b){
-		temp=*a;
-		*a=*b;
-		*b=temp;
+	if (*a > *b) {
+		temp = *a;
+		*a = *b;
+		*b = temp;
 	}
 }
-void decrease(double *a,double *b){
+void decrease(double *a, double *b){
 	double temp;
-	if(*a<*b){
-		temp=*a;
-		*a=*b;
-		*b=temp;
+	if(*a < *b){
+		temp = *a;
+		*a = *b;
+		*b = temp;
 	}
 }
