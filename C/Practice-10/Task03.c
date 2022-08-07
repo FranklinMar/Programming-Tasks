64a88e95847811956e661e0f71eed632684e5e41
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-07 19:44:24 +0300

    Update Task03.c

--- C/Practice-10/Task03.c
+++ C/Practice-10/Task03.c
@@ -1,23 +1,22 @@
 /*
---------------
+----------------
 Name: Task03.c
---------------
-Description: Write a program, that will have
-next functions:
+------------------------------------------------------------
+Description: Write a program, that will have next functions:
 
- void show1(const double arr1[], int n);
- void show2(const double arr2[][3], int n);
+- void show1(const double arr1[], int n);
+- void show2(const double arr2[][3], int n);
  
-where inside show1() n – number of elements of array,
-and inside show2() n – number of rows of array.
+  where inside show1() n – number of elements of array, and 
+  inside show2() n – number of rows of array.
 
-Also, implement next goals:
+  Also, implement next goals:
 a) when calling function show1(), it must have compound literals, 
-that consists of values 8, 3, 9; and then 3.
+   that consists of values 8, 3, 9; and then 3.
 b) when calling function show2(), it must have compound literals,
-that consists of values 8, 3, 9 - values for the first row, and of
-values 5, 4, 1 - values for the second row (and then 2).
---------------
+   that consists of values 8, 3, 9 - values for the first row, 
+   and of values 5, 4, 1 - values for the second row (and then 2).
+------------------------------------------------------------
 */
 #include <stdio.h>
 #define N 3
