4618a57c3d8d63efb8521db3b436c07d1cd3019c
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-08 23:59:18 +0300

    Create Task09.cpp

---
+++ C++/Practice-3/Task09.cpp
@@ -0,0 +1,21 @@
+/*
+------------------
+Name: Task01.cpp
+------------------------------------------------------------
+Description: Create a "who" class. The "who" constructor 
+  must have one character argument to be used to identify 
+  the object. While creating the object's constructor 
+  should display the message: 
+  
+  Constructing who #x
+  
+  where x - identification character, unique for each object.
+  While deleting the object, it's destructor should display 
+  the message: 
+  
+  Destroying who #x
+  
+  where x - identification character, unique for each object, 
+  again.
+------------------------------------------------------------
+*/
