7e66249455d9131e11f61ceed8e902fa8a584525
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-08 18:36:20 +0300

    Create Task18.cpp

---
+++ C++/Practice-2/Task18.cpp
@@ -0,0 +1,13 @@
+/*
+------------------
+Name: Task18.cpp
+------------------------------------------------------------
+Description: Create a class "mybox", that recieves 3 numbers 
+  of double type by constructor. They are representing the 
+  length of the sides of parallelepiped. Class mybox must
+  calculate it's volume and store the result in the double 
+  type variable as well. Include a method volume(), that 
+  will print on the display the volume of any instance of 
+  "mybox" type.
+------------------------------------------------------------
+*/
