a451235f931e0357bf1c620425bd25f2b802fcfc
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-08 17:14:41 +0300

    Create Task08.cpp

---
+++ C++/Practice-2/Task08.cpp
@@ -0,0 +1,32 @@
+/*
+------------------
+Name: Task08.cpp
+------------------------------------------------------------
+Description: Create a function myroot(), which returns 
+  the square root of its argument. Overload myroot() in 
+  three ways (look Unit 2 "Introducing Function Overloading"):
+  to get square root of integer, long integer and double 
+  number. (For calculating square root you can use sqrt() 
+  from standart library.
+------------------------------------------------------------
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int myroot(int a) {
+	return sqrt(a);
+}
+long int myroot(long int a) {
+	return sqrt(a);
+}
+double myroot(double a) {
+	return sqrt(a);
+}
+
+int main() {
+	cout << "Square root of 100:" << myroot(100) << "\n";
+	cout << "Square root of 100L:" << myroot(100L/* 100000000*/) << "\n";
+	cout << "Square root of 56,25:" << myroot(56.25) << "\n";
+	return 0;
+}
