9b03e987eadc57af2fdd70983bc7214c435e6aa9
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-14 18:34:57 +0300

    Create Task05.cpp

---
+++ C++/Practice-5/Task05.cpp
@@ -0,0 +1,36 @@
+/*
+------------------
+Name: Task05.cpp
+------------------------------------------------------------
+Description: Write a program that uses the "new" operator to
+  allocate dynamic memory to float, long and char variables. 
+  Give values to those dynamic variables and print those 
+  values to the screen. At the end, free all dynamically 
+  allocated variables (with dynamic memory) with delete 
+  operator.
+------------------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	float *floatp;
+	long *longp;
+	char *charp;
+	floatp = new float;
+	longp = new long;
+	charp = new char;
+	if (!floatp || !longp || !charp) {
+		cout << "Allocation error \n";
+		return 0;
+	}
+	*floatp = 6.5;
+	*longp = 600000;
+	*charp = 'A';
+	cout << "\nFloat:" << *floatp << "\nLong:" << *longp << "\nChar:" << *charp;
+	delete floatp;
+	delete longp;
+	delete charp;
+	return 0;
+}
