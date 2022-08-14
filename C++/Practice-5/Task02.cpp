c60a41c8db7b7b9c6fa59c6d5ab1fcc7bdfbc656
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-14 18:20:54 +0300

    Create Task02.cpp

---
+++ C++/Practice-5/Task02.cpp
@@ -0,0 +1,10 @@
+/*
+------------------
+Name: Task02.cpp
+------------------------------------------------------------
+Description: Using the following class declaration, create 
+  an array of 10 elements, initialize the variable "num" 
+  with values from 1 to 10, and the variable "sqr" - with 
+  squared num.
+------------------------------------------------------------
+*/
