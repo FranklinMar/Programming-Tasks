af556166140816e65046e66b4b24fdffd30641be
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-15 19:36:09 +0300

    Create Task05.cpp

---
+++ C++/Practice-8/Task05.cpp
@@ -0,0 +1,12 @@
+/*
+------------------
+Name: Task05.cpp
+------------------------------------------------------------
+Description: If the variables 'a' and 'b' inside the "myclass" 
+  class Task 04 weren't declared "private" (by default) but 
+  "protected" members, would any of your answers to the question 
+  of previous exercise change? If yes, why?
+------------------------------------------------------------
+*/
+/* Answer - NO, because the problem lies not in the access 
+  to 'a' and 'b' variables, but inside initalization.*/
