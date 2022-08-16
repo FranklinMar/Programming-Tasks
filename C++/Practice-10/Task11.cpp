1e19d7978d086f6066a9347ed837f4630b5255cf
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-16 17:08:15 +0300

    Create Task11.cpp

---
+++ C++/Practice-10/Task11.cpp
@@ -0,0 +1,22 @@
+/*
+------------------
+Name: Task11.cpp
+------------------------------------------------------------
+Description: Given the following class. Write a program to 
+  output the content of an object of this class into a file:
+
+  Hint: Create custom output function.
+  
+class account {
+ int custnum;
+ char name[80];
+ double balance;
+public:
+ account(int c, char *n, double b) {
+ custom = c;
+ strcpy(name, n);
+ balance = b;
+ }
+};
+------------------------------------------------------------
+*/
