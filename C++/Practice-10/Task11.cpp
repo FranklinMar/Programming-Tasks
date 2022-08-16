6a07ca8946a0d49c05eadd8b08fe93a6e8846239
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2022-08-16 17:10:16 +0300

    Update Task11.cpp

--- C++/Practice-10/Task11.cpp
+++ C++/Practice-10/Task11.cpp
@@ -20,3 +20,39 @@ public:
 };
 ------------------------------------------------------------
 */
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+class account {
+  int custnum;
+  char name[80];
+  double balance;
+public:
+  account(int c, char *n, double b) {
+    custnum = c;
+    strcpy(name, n);
+    balance = b;
+  }
+  friend ostream &operator <<(ostream &o, account &obj);
+};
+
+ostream &operator <<(ostream &o, account &obj) {
+	o << "Cust-num :"<< obj.custnum << " , Name :" << obj.name << " , Balance :" << obj.balance << endl;
+	return o;
+}
+
+int main() {
+	account bank1(201401, "Arkas", 2501);
+	account bank2(250101, "Car", 6500);
+	ofstream fout("text11.txt");
+	if (!fout) {
+		cout << "Cannot open text11.txt file.\n";
+		return 1;
+	}
+	fout << bank1 << bank2;
+	cout << bank1 << bank2;
+	fout.close();
+	return 0;
+}
